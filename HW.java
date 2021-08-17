d5bdc0b8ca1f62238498e4a69f098cf3632ee0c9
MikeLSDET <mikesdetlopez@gmail.com>
2021-08-17 11:06:35 -0400

    now only prints even numbers

--- HW.java
+++ HW.java
@@ -6,8 +6,10 @@ public class HW {
 		// TODO Auto-generated method stub
 		
 		for (int i=1; i<101; i++) {
+			if(i%2==0) {
 			
-			System.out.print(i+" ");
+			System.out.print(i+" ");}
+			
 		}
 
 	}
