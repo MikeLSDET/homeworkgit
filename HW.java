53aa802396be7ea37ac2829be99994816fcc745c
MikeLSDET <mikesdetlopez@gmail.com>
2021-08-17 10:54:03 -0400

    this was hw

---
+++ HW.java
@@ -0,0 +1,15 @@
+package HomeWork;
+
+public class HW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i=1; i<11; i++) {
+			
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
